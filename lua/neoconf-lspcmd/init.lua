70606b86516d8f232638979223cf3af00c532f93
Ryan Schlesinger <ryan@ryanschlesinger.com>
2023-04-27 11:11:03 -0300

    Convert to using neoconf options for plugin

--- lua/neoconf-lspcmd/init.lua
+++ lua/neoconf-lspcmd/init.lua
@@ -1,4 +1,6 @@
 local Util = require("neoconf.util")
+local Config = require("neoconf.config")
+
 local logger = require("plenary.log").new({
 	plugin = "neoconf-lspcmd",
 	level = "info",
@@ -6,26 +8,38 @@ local logger = require("plenary.log").new({
 
 local M = {}
 
-M.ls_mappings = {
-	standardrb = "standardRuby",
+M.defaults = {
+	enabled = true,
+	log_level = "info",
+	ls_mappings = {
+		standardrb = "standardRuby",
+	},
 }
 
-function M.on_schema(schema) end
+function M.on_schema(_) end
 
 function M.setup()
+	Config.options.plugins.lspcmd = Util.merge(M.defaults, Config.options.plugins.lspcmd)
+	logger.level = Config.options.plugins.lspcmd.log_level
+
 	Util.on_config({
-		name = "lspcmd",
+		name = "settings/plugins/lspcmd",
 		on_config = M.on_new_config,
 	})
 end
 
-function M.on_new_config(config)
-	-- logger.debug("on_new_config", config, root_dir, original_config)
+function M.on_new_config(config, root_dir, original_config)
+	logger.debug("on_new_config", config, root_dir, original_config)
 
 	logger.debug("config.settings", config.settings)
 
+	local options = Config.get({ file = root_dir })
+	if not options.plugins.lspcmd.enabled then
+		return
+	end
+
 	local name = config["name"]
-	local configName = M.ls_mappings[name] or name
+	local configName = options.plugins.lspcmd.ls_mappings[name] or name
 	logger.debug("configName", configName)
 
 	local commandPath = vim.tbl_get(config.settings, configName, "commandPath")
